7646b41d95d096df734357e7c623763a6c51723f
jcredding <TempestTTU@gmail.com>
2013-08-10 12:45:29 -0500

    Make `Qs::Daemon` a mixin, not a class
    This is a small change to make the `Qs::Daemon` more like the
    `Deas::Server`, a mixin that is used to define a daemon. This will
    be changed more to work like the `Deas::Server` but this change
    revealed an issue with the file evaluation. This switches our
    file evaluation to use Rack's trick of evaluating the file's
    contents into a proc and then instance evaluating that. This
    produces more expected behavior. The previous implementation had
    the side effect of defining constants namespaced to the instance
    of the `Config` that evaluated them. This was very confusing in
    the tests and could have other issues, so I decided to switch it
    to the less-simple implemenation from Rack. I believe this will
    produce more expected results and not cause any strange errors
    down the road.

--- lib/qs/daemon.rb
+++ lib/qs/daemon.rb
@@ -1,92 +1,89 @@
-module Qs
+module Qs; end
+module Qs::Daemon
 
-  class Daemon
+  # This is all temporary
+  attr_accessor :queue_name, :logger, :pid_file, :redis_ip, :redis_port
+  attr_accessor :thread, :state
 
-    # This is all temporary
-    attr_accessor :queue_name, :logger, :pid_file, :redis_ip, :redis_port
-    attr_accessor :thread, :state
+  def initialize
+    set_state :init
+    @signal_queue ||= []
+  end
 
-    def initialize
-      set_state :init
-      @signal_queue ||= []
-    end
-
-    def run
-      set_state :run
-      @thread = Thread.new do
-        loop do
-          break if !@state.run?
-          sleep 0.5
-          handle_signal(@signal_queue.pop) unless @signal_queue.empty?
-        end
+  def run
+    set_state :run
+    @thread = Thread.new do
+      loop do
+        break if !@state.run?
+        sleep 0.5
+        handle_signal(@signal_queue.pop) unless @signal_queue.empty?
       end
     end
+  end
 
-    def join_thread
-      @thread.join
+  def join_thread
+    @thread.join
+  end
+
+  def signal_stop
+    @signal_queue << :stop
+  end
+
+  def signal_halt
+    @signal_queue << :halt
+  end
+
+  def signal_restart
+    @signal_queue << :restart
+  end
+
+  def running?
+    @thread && @thread.alive?
+  end
+
+  def in_stop_state?
+    @state.stop?
+  end
+
+  def in_halt_state?
+    @state.halt?
+  end
+
+  def in_restart_state?
+    @state.restart?
+  end
+
+  private
+
+  def handle_signal(signal)
+    set_state(signal)
+  end
+
+  def set_state(name)
+    @state = State.new(name)
+  end
+
+  class State
+    def initialize(name)
+      @name = name.to_sym
+      # TODO - validation
     end
 
-    def signal_stop
-      @signal_queue << :stop
+    def run?
+      @name == :run
     end
 
-    def signal_halt
-      @signal_queue << :halt
+    def restart?
+      @name == :restart
     end
 
-    def signal_restart
-      @signal_queue << :restart
+    def stop?
+      @name == :stop
     end
 
-    def running?
-      @thread && @thread.alive?
+    def halt?
+      @name == :halt
     end
-
-    def in_stop_state?
-      @state.stop?
-    end
-
-    def in_halt_state?
-      @state.halt?
-    end
-
-    def in_restart_state?
-      @state.restart?
-    end
-
-    private
-
-    def handle_signal(signal)
-      set_state(signal)
-    end
-
-    def set_state(name)
-      @state = State.new(name)
-    end
-
-    class State
-      def initialize(name)
-        @name = name.to_sym
-        # TODO - validation
-      end
-
-      def run?
-        @name == :run
-      end
-
-      def restart?
-        @name == :restart
-      end
-
-      def stop?
-        @name == :stop
-      end
-
-      def halt?
-        @name == :halt
-      end
-    end
-
   end
 
 end
