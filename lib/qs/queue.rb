1a8634d4687cd1ff2c6621c5b640ebfead581f0a
jcredding <TempestTTU@gmail.com>
2013-08-28 01:36:21 -0500

    Update daemon config and `Process` interaction
    This cleans up and formalizes some of the `Daemon` interface,
    specifically where it interacts with the `Process`. This was
    originally hacked in to make the `Process` work as needed. The
    options that the `Process` class uses are now set using the DSL
    and others are properly pulled from the `Queue`. This revealed
    that the `queue`, `init_procs` and `error_procs` for a daemon's
    configuration were not being copied down from the class to the
    instance correctly. This sets up updates to the signal handling,
    which affects the `Process` class.

--- lib/qs/queue.rb
+++ lib/qs/queue.rb
@@ -2,7 +2,7 @@ module Qs; end
 class Qs::Queue
 
   # This is temporary, just defining what a daemon needs from a queue
-  attr_accessor :mappings
+  attr_accessor :name, :logger, :mappings
 
   def initialize
     @mappings = []
