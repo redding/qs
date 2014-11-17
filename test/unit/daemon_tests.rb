61c827d8fea52c2c9d092d3aef4446a209d6f160
Collin Redding <TempestTTU@gmail.com>
2014-11-17 16:25:56 -0600

    Add `Daemon::Configuration`
    This adds the configuration class for daemons which is used to
    store config values when defining a `Daemon`. When the `Daemon`
    is initialized, the `Configuration` will be validated and then
    a `DaemonData` will be built from it, which is used for "reading"
    the config values. The `Configuration` provides a nice way to
    write values through a DSL on the `Daemon`, and the `DaemonData`
    is used for reading those values efficiently. When the
    `Configuration` is initialized, the init procs configured for a
    `Daemon` are run and it ensures it has enough information for a
    `Daemon` to be run.
    
    This also removes all the old `Daemon` logic that was previously
    written. This logic was as a prototype for Qs. This is throwing
    away the prototype to implement the logic using Sanford as
    reference.

--- test/unit/daemon_tests.rb
+++ test/unit/daemon_tests.rb
@@ -1,247 +1,124 @@
 require 'assert'
 require 'qs/daemon'
 
-require 'dat-worker-pool/worker_pool_spy'
-require 'thread'
+require 'ns-options/assert_macros'
+require 'qs/queue'
 
 module Qs::Daemon
 
   class UnitTests < Assert::Context
     desc "Qs::Daemon"
     setup do
-      @shutdown_timeout = 15
       @daemon_class = Class.new{ include Qs::Daemon }
-      @daemon_class.shutdown_timeout @shutdown_timeout
-      @queue = @daemon_class.configuration.queue
-      @daemon = @daemon_class.new
     end
-    subject{ @daemon }
-
-    should have_cmeths :configuration
-    should have_cmeths :queue
-    should have_cmeths :pid_file
-    should have_cmeths :min_workers, :max_workers, :workers
-    should have_cmeths :wait_timeout, :shutdown_timeout
-
-    should have_readers :queue_name, :pid_file, :logger
-    should have_writers :check_for_signals_proc
-
-    should have_imeths :start, :stop, :halt
-    should have_imeths :running?
-
-    should "return it's configuration's pid file with #pid_file" do
-      assert_equal @daemon_class.configuration.pid_file, subject.pid_file
-    end
-
-    should "return it's configuration's queue's name with #queue_name" do
-      assert_equal @queue.name, subject.queue_name
-    end
-
-    should "return it's queue's logger with #logger" do
-      assert_equal @queue.logger, subject.logger
-    end
-
-    should "be stopped by default" do
-      assert_not subject.running?
-    end
-
-  end
-
-  class StartingAndStoppingTests < UnitTests
-    setup do
-      @worker_pool_spy = DatWorkerPool::WorkerPoolSpy.new
-      Assert.stub(DatWorkerPool, :new).with(
-        @daemon_class.min_workers,
-        @daemon_class.max_workers
-      ){ @worker_pool_spy }
-    end
-    teardown do
-      @daemon.stop rescue false
-    end
-
-  end
-
-  class StartTests < StartingAndStoppingTests
-    desc "when started"
-    setup do
-      @thread = @daemon.start
-    end
-
-    should "be running" do
-      assert subject.running?
-    end
-
-    should "return the daemon's work loop thread" do
-      assert_instance_of Thread, @thread
-      assert @thread.alive?
-    end
-
-  end
-
-  class CheckSignalProcTests < StartingAndStoppingTests
-    desc "when given a check for signals proc and started"
-    setup do
-      @daemon_class.wait_timeout 0
-      @daemon = @daemon_class.new
-      @check_for_signals_proc_called = 0
-      @daemon.check_for_signals_proc = proc{ @check_for_signals_proc_called += 1 }
-      @thread = @daemon.start
-    end
-
-    should "call `check_for_signals_proc` as part of it's work loop" do
-      @thread.join(0.1)
-      assert @check_for_signals_proc_called > 0
-    end
-
-  end
-
-  class WorkLoopWithWorkTests < StartingAndStoppingTests
-    desc "when started with a worker available and work on the queue, it"
-    setup do
-      @worker_pool_spy.worker_available = true
-      Assert.stub(@queue, :fetch_job).with(
-        @daemon_class.configuration.wait_timeout
-      ){ 'test' }
-      @thread = @daemon.start
-    end
-
-    should "fetch jobs from the queue and add them to the worker pool" do
-      @thread.join(0.1)
-      assert_includes 'test', @worker_pool_spy.work_items
-    end
-
-  end
-
-  class WorkLoopNoWorkersAvailableTests < StartingAndStoppingTests
-    desc "when it's started but there are no workers available"
-    setup do
-      @worker_pool_spy.worker_available = false
-      Assert.stub(@queue, :fetch_job){ raise "shouldn't be called" }
-      @thread = @daemon.start
-    end
-
-    should "not add any jobs to the worker pool" do
-      @thread.join(0.1)
-      assert_equal 0, @worker_pool_spy.work_items.size
-    end
-
-  end
-
-  class WorkLoopQueueNotEmptyTests < StartingAndStoppingTests
-    desc "when it's started but the worker pool's queue isn't empty"
-    setup do
-      @worker_pool_spy.add_work 'job'
-      Assert.stub(@queue, :fetch_job){ raise "shouldn't be called" }
-      @thread = @daemon.start
-    end
-
-    should "not fetch any jobs and add them to the worker pool" do
-      @thread.join(0.1)
-      assert_equal 1, @worker_pool_spy.work_items.size
-    end
-
-  end
-
-  class StopTests < StartingAndStoppingTests
-    desc "when stopped"
-    setup do
-      @thread = @daemon.start
-      @daemon.stop true
-    end
-
-    should "have shutdown the worker pool" do
-      assert @worker_pool_spy.shutdown_called
-      assert_equal @shutdown_timeout, @worker_pool_spy.shutdown_timeout
-    end
-
-    should "no longer be running" do
-      assert_not subject.running?
-    end
-
-    should "stop the daemon's work loop thread" do
-      assert_not @thread.alive?
-    end
-
-  end
-
-  class HaltTests < StartingAndStoppingTests
-    desc "when halted"
-    setup do
-      @thread = @daemon.start
-      @daemon.halt true
-    end
-
-    should "not have shutdown the worker pool" do
-      assert_not @worker_pool_spy.shutdown_called
-    end
-
-    should "no longer be running" do
-      assert_not subject.running?
-    end
-
-    should "stop the daemon's work loop thread" do
-      assert_not @thread.alive?
-    end
-
-  end
-
-  class ValidateConfigurationTests < UnitTests
-    desc "with an invalid config"
-    setup do
-      Assert.stub(@daemon_class.configuration, :validate!){ raise StandardError }
-    end
-
-    should "validate the configuration when initialized" do
-      assert_raises(StandardError){ @daemon_class.new }
-    end
-
-  end
-
-  class ClassMethodTests < UnitTests
-    desc "class"
     subject{ @daemon_class }
 
-    should "return an instance of a Configuration with #configuration" do
-      assert_instance_of Configuration, subject.configuration
+  end
+
+  class ConfigurationTests < UnitTests
+    include NsOptions::AssertMacros
+
+    desc "Configuration"
+    setup do
+      @queue = Qs::Queue.new do
+        name Factory.string
+        job_handler_ns 'Qs::Daemon'
+        job 'test', 'TestHandler'
+      end
+
+      @configuration = Configuration.new.tap do |c|
+        c.name Factory.string
+        c.queues << @queue
+      end
+    end
+    subject{ @configuration }
+
+    should have_options :name, :pid_file
+    should have_options :min_workers, :max_workers
+    should have_options :verbose_logging, :logger
+    should have_options :shutdown_timeout
+    should have_accessors :init_procs, :error_procs
+    should have_accessors :queues
+    should have_imeths :routes
+    should have_imeths :to_hash
+    should have_imeths :valid?, :validate!
+
+    should "be an ns-options proxy" do
+      assert_includes NsOptions::Proxy, subject.class
     end
 
-    should "allow reading/writing the configuration's queue" do
-      test_queue = Qs::Queue.new
-      assert_nothing_raised{ subject.queue test_queue }
-      assert_equal test_queue, subject.queue
+    should "default its options" do
+      config = Configuration.new
+      assert_nil config.name
+      assert_nil config.pid_file
+      assert_equal 1, config.min_workers
+      assert_equal 4, config.max_workers
+      assert_true config.verbose_logging
+      assert_instance_of Qs::NullLogger, config.logger
+      assert_equal [], config.init_procs
+      assert_equal [], config.error_procs
+      assert_equal [], config.queues
+      assert_equal [], config.routes
     end
 
-    should "allow reading/writing the configuration's pid file" do
-      assert_nothing_raised{ subject.pid_file 'test.pid' }
-      assert_equal 'test.pid', subject.pid_file
+    should "not be valid by default" do
+      assert_false subject.valid?
     end
 
-    should "allow reading/writing the configuration's min workers" do
-      assert_nothing_raised{ subject.min_workers 2 }
-      assert_equal 2, subject.min_workers
+    should "know its routes" do
+      assert_equal subject.queues.map(&:routes).flatten, subject.routes
     end
 
-    should "allow reading/writing the configuration's max workers" do
-      assert_nothing_raised{ subject.max_workers 2 }
-      assert_equal 2, subject.max_workers
+    should "include its error procs, queue redis keys and routes in its hash" do
+      config_hash = subject.to_hash
+      assert_equal subject.error_procs, config_hash[:error_procs]
+      expected = subject.queues.map(&:redis_key)
+      assert_equal expected, config_hash[:queue_redis_keys]
+      assert_equal subject.routes, config_hash[:routes]
     end
 
-    should "allow setting both it's min and max workers with #workers" do
-      assert_nothing_raised{ subject.workers 3 }
-      assert_equal 3, subject.min_workers
-      assert_equal 3, subject.max_workers
+    should "call its init procs when validated" do
+      called = false
+      subject.init_procs << proc{ called = true }
+      subject.validate!
+      assert_true called
     end
 
-    should "allow reading/writing the configuration's wait timeout" do
-      assert_nothing_raised{ subject.wait_timeout 1 }
-      assert_equal 1, subject.wait_timeout
+    should "ensure its required options have been set when validated" do
+      subject.name = nil
+      assert_raises(InvalidError){ subject.validate! }
+      subject.name = Factory.string
+
+      subject.queues = []
+      assert_raises(InvalidError){ subject.validate! }
+      subject.queues << @queue
+
+      assert_nothing_raised{ subject.validate! }
     end
 
-    should "allow reading/writing the configuration's shutdown timeout" do
-      assert_nothing_raised{ subject.shutdown_timeout 15 }
-      assert_equal 15, subject.shutdown_timeout
+    should "validate its routes when validated" do
+      subject.routes.each{ |route| assert_nil route.handler_class }
+      subject.validate!
+      subject.routes.each{ |route| assert_not_nil route.handler_class }
+    end
+
+    should "be valid after being validated" do
+      assert_false subject.valid?
+      subject.validate!
+      assert_true subject.valid?
+    end
+
+    should "only be able to be validated once" do
+      called = 0
+      subject.init_procs << proc{ called += 1 }
+      subject.validate!
+      assert_equal 1, called
+      subject.validate!
+      assert_equal 1, called
     end
 
   end
+
+  TestHandler = Class.new
 
 end
