a876052362a67829aff934cef60eb00edb6e404e
Collin Redding <TempestTTU@gmail.com>
2015-05-21 12:53:54 -0500

    Add `EventHandler` mixin
    This adds an `EventHandler` mixin for defining handlers for
    processing an event. This is part of setting up an events system
    in Qs.
    
    The event handler extends the job handler functionality and uses
    the job from its runner to build an `Event`. The params method
    on an event handler returns its event params not the job params.
    The event handler also provides access to the event itself similar
    to how the job handler provides access to the job.

--- test/support/factory.rb
+++ test/support/factory.rb
@@ -1,4 +1,6 @@
 require 'assert/factory'
+require 'qs/job'
+require 'qs/event'
 
 module Factory
   extend Assert::Factory
@@ -9,6 +11,21 @@ module Factory
     exception = nil
     begin; raise(klass, message); rescue StandardError => exception; end
     exception
+  end
+
+  def self.job(params = nil)
+    name       = Factory.string
+    params     = { Factory.string => Factory.string }
+    created_at = Factory.time
+    Qs::Job.new(name, params, created_at)
+  end
+
+  def self.event_job(params = nil)
+    channel      = Factory.string
+    event        = Factory.string
+    params       = { Factory.string => Factory.string }
+    published_at = Factory.time
+    Qs::Event.build(channel, event, params, published_at).job
   end
 
 end
